1849c13e035ebd19aee1e109760b01cf3380fa75
igor-hara <munkieryder@gmail.com>
2024-05-02 10:30:25 +0200

    add: signOut, protected routes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -208,6 +208,10 @@ const Navbar = () => {
                     Saved Properties
                   </Link>
                   <button
+                    onClick={() => {
+                      setIsMobileProfileMenuOpen(false)
+                      signOut()
+                    }}
                     className='block px-4 py-2 text-sm text-gray-700'
                     role='menuitem'
                     tabIndex='-1'
