2cc089a30bf0ab5555ab6db8efb2c54d368b337f
igor-hara <munkieryder@gmail.com>
2024-05-02 09:54:04 +0200

    add: login button to goolge login page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -18,11 +18,12 @@ const Navbar = () => {
 
   const pathname = usePathname()
 
+  const setAuthProviders = async () => {
+    const res = await getProviders()
+    setProviders(res)
+  }
+
   useEffect(() => {
-    const setAuthProviders = async () => {
-      const res = await getProviders()
-      setProviders(res)
-    }
     setAuthProviders()
   }, [])
   // console.log('session: ', session)
@@ -240,12 +241,18 @@ const Navbar = () => {
               Add Property
             </Link>
           )}
-          {!session && (
-            <button className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4'>
-              <FaGoogle className='text-white mr-2' />
-              <span>Login or Register</span>
-            </button>
-          )}
+          {!session &&
+            providers &&
+            Object.values(providers).map((provider, ind) => (
+              <button
+                onClick={() => signIn(provider.id)}
+                key={ind}
+                className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
+              >
+                <FaGoogle className='text-white mr-2' />
+                <span>Login or Register</span>
+              </button>
+            ))}
         </div>
       </div>
     </nav>
